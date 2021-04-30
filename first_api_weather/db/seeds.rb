59ca7af919a424a0a546ffa9dd85e027ba6faee5
Caroline Salib <carolinesalibc@gmail.com>
2021-04-29 21:25:13 -0600

    Add locations API

--- first_api_weather/db/seeds.rb
+++ first_api_weather/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+l = Location.create(name: 'New York City')
+l.recordings.create(temperature: 32, status: 'cloudy')
+l.recordings.create(temperature: 34, status: 'sunny')
+l.recordings.create(temperature: 28, status: 'sunny')
+l.recordings.create(temperature: 10, status: 'cloudy')
+l.recordings.create(temperature: 22, status: 'cloudy')
+l.recordings.create(temperature: 5, status: 'rainy')
